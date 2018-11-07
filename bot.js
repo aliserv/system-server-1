56dbdccd17beddd2d893cbf6d9291ca84f7d72ca
aliserv <44829784+aliserv@users.noreply.github.com>
2018-11-07 23:32:39 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+
+client.on('ready', async() => {
+var server = "509702399766822923"; // ايدي السررفر
+var channel = "509702399766822925";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **')
+    },305);
+})
+
+client.login(NTA4MzU2MTQzMTM1OTgxNTgw.Dr-MNA.y2zqqq380oG8_-gpnslVSpG0ACk);
