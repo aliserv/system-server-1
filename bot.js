7c06b4133f88b82862a2195a3cbb5f09cab9f0a3
aliserv <44829784+aliserv@users.noreply.github.com>
2018-11-07 20:48:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '#';
 client.on('message', message => {
     if (message.content === 'استغفر الله العظيم الله اكبر الله اكبر') {
           let count = 0;
           let ecount = 0;
           for(let x = 0; x < 500; x++) {
-            message.channel.send(استغفر الله العظيم الله اكبر الله اكبر)
+            message.channel.send(general)
               .then(m => {
                 count++;
               })
