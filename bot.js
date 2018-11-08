3cd52b955a55aa782a1d7d80e22361930013446c
aliserv <44829784+aliserv@users.noreply.github.com>
2018-11-08 02:52:54 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const adminprefix = "#";
+const devs = ['509392266763173891'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+if (message.content.startsWith(adminprefix + 'set')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/prflasg18");
+    message.channel.sendMessage(`**تم تغيير تويتش الحساب إلى  ${argresult}**`)
+}
+});
